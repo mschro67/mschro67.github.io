1da0149df928ec7c7c66bc8e3cf97ac8d961fcc7
mschro67 <maximilian.s107@evt.schule.koeln>
2026-01-21 14:19:02 +0000

    Fix canvas.js

--- canvas.js
+++ canvas.js
@@ -1,37 +1,35 @@
-//by mschro67 with Gemini
-//last change: Jan 18 2026
+//by mschro67
+//made with Gemini
+//last change: Jan 21 2026
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-//const matrixCharsJapanese = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヰヱヲン";
-//const matrixChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+const matrixCharsJapanese = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヰヱヲン";
+const matrixChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
 const binary="10";
 
-const characters = binary.split("");
-
+const characters = binary.split('');
 const fontSize = 16;
-const columns = canvas.width / fontSize + 1;
-
+const columns = canvas.width / fontSize;
 const drops = [];
 
 for (let i = 0; i < columns; i++) {
     drops[i] = 1;
 }
 
-/*
-ctx.fillStyle = '#000';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-*/
+//ctx.fillStyle = '#000';
+//ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 
 function draw() {
-    //ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.font = fontSize + "px monospace";
-    ctx.fillStyle = "#0F0";
+
+    ctx.fillStyle = '#0F0';
+    ctx.font = fontSize + 'px monospace';
 
     for (let i = 0; i < drops.length; i++) {
         const text = characters[Math.floor(Math.random() * characters.length)];
@@ -50,9 +48,9 @@ function draw() {
 
 setInterval(draw, 50);
 
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 });
