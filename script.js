0631fcae8310cdc996d9c00996b019d7d71eb004
mschro67 <maximilian.s107@evt.schule.koeln>
2026-01-22 18:38:44 +0000

    Add functons to script.js and add test.html

---
+++ script.js
@@ -0,0 +1,26 @@
+//by mschro67
+//last change: Jan 22 2025
+
+const main=document.getElementById("main");
+const experiments=document.getElementById("experiments");
+const stuff=document.getElementById("stuff");
+
+function showAll(){
+    main.removeAttribute("hidden");
+    experiments.removeAttribute("hidden");
+    stuff.removeAttribute("hidden");
+}
+
+function hideAll(){
+    showAll();
+    main.toggleAttribute("hidden");
+    experiments.toggleAttribute("hidden");
+    stuff.toggleAttribute("hidden");
+}
+
+function show(section){
+    hideAll();
+    document.getElementById(section).removeAttribute("hidden");
+}
+
+show("main");
