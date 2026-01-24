341c3c802d46adeae5f42341c96fd0219a084ae4
mschro67 <maximilian.s107@evt.schule.koeln>
2026-01-24 13:39:34 +0000

    Make data.js automaticly adjust to the data

--- data/data.js
+++ data/data.js
@@ -1,69 +1,65 @@
 //by mschro67
 //last change: Jan 24 2026
 
-import people from "/data/people.json" with { type: "json" };
+import data from "/data/data.json" with { type: "json" };
 
-let ages={};
-let jobs={};
-let names={};
-let hobbies={};
-const persons=people.length;
-let ages_avg=0;
+let options={}
+const persons=data.length;
+
+const style=document.getElementById("style");
+const selected=document.getElementById("selected");
+const color=document.getElementById("color");
+const out=document.getElementById("out");
 
 function table(header,data) {
-    let sorted=Object.entries(data).sort((a, b) => b[1] - a[1]);
-    data=Object.fromEntries(sorted);
-    let table=`<table><tr><th>${header.charAt(0).toUpperCase() + header.slice(1)}</th><th>Proportion</th><th>People</th><th>Percent</th></tr>`;
+    let table=`<table><tr><th>${header.charAt(0).toUpperCase() + header.slice(1).replace("_"," ")}</th><th>Proportion</th><th>People</th><th>Percent</th></tr>`;
     for (const key in data) {
-        table+=`<tr><td>${key}</td><td><progress value=${data[key]/persons} max=1></progress></td><td>${data[key]}</td><td>${data[key]/persons*100}%</td></tr>`;
+        table+=`<tr><td>${key}</td><td><progress value=${data[key]/persons} max=1></progress></td><td>${data[key]}</td><td>${data[key]/persons*100}<span style="color:${color.value};">%</span></td></tr>`;
     }
     table+="</table>";
     return table;
 }
 
-for (const person of people) {
-    if (ages[person.age]) {
-        ages[person.age] += 1;
-    } else {
-        ages[person.age] = 1;
+function display(){
+    if (selected.value==="-"){
+        out.innerHTML=`<span style='color:${color.value};'>select data</span>`;
+    }else{
+        out.innerHTML=table(selected.value,options[selected.value]);
+        style.innerHTML=`progress::-webkit-progress-value {background-color:${color.value};}progress::-moz-progress-bar {background-color:${color.value};}th{border:solid 1px ${color.value}}`;
     }
-    ages_avg+=person.age;
-    if (jobs[person.occupation]) {
-        jobs[person.occupation] += 1;
-    } else {
-        jobs[person.occupation] = 1;
+}
+
+function addValue(option,value){
+    if (!options[option]){
+        options[option] = {};
     }
-    if (names[person.name.split(" ")[0]]) {
-        names[person.name.split(" ")[0]] += 1;
+
+    if (options[option][value]) {
+        options[option][value] += 1;
     } else {
-        names[person.name.split(" ")[0]] = 1;
+        options[option][value] = 1;
     }
-    for (const hobby of person.hobbies) {
-        if (hobbies[hobby]) {
-            hobbies[hobby] += 1;
-        } else {
-            hobbies[hobby] = 1;
+}
+
+function loadOptions(){
+    for (const option in options){
+        selected.innerHTML+=`<option>${option}</option>`;
+    }
+}
+
+
+for (const person of data) {
+    for (const option in person){
+        if (Array.isArray(person[option])){
+            for (const item of person[option]){
+                addValue(option,item)
+            }
+        }else{
+            addValue(option,person[option])
         }
     }
 }
 
-ages_avg/=persons;
-const categories={
-    "age": ages,
-    "occupation": jobs,
-    "name": names,
-    "hobby": hobbies
-}
-
-function display(){
-    if (document.getElementById("selected").value==="-"){
-        document.getElementById("container").innerHTML="<span style='color:#777;'>select data</span>";
-    }else{
-        const selected=document.getElementById("selected").value;
-        document.getElementById("container").innerHTML=table(selected,categories[selected]);
-        document.getElementById("style").innerHTML=`progress::-webkit-progress-value {background-color:${document.getElementById("color").value};}progress::-moz-progress-bar {background-color:${document.getElementById("color").value};}`;
-    }
-}
-
+loadOptions();
+display();
 window.display=display;
-window.onload=display;
