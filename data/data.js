401f621f084ad69e7ee627ca97e43c4e40e64877
mschro67 <maximilian.s107@evt.schule.koeln>
2026-01-24 11:46:20 +0000

    Update website

--- data/data.js
+++ data/data.js
@@ -6,11 +6,14 @@ import people from "/data/people.json" with { type: "json" };
 let ages={};
 let jobs={};
 let names={};
+let hobbies={};
 const persons=people.length;
 let ages_avg=0;
 
 function table(header,data) {
-    let table=`<table><tr><th>${header}</th><th>Proportion</th><th>People</th><th>Percent</th></tr>`;
+    let sorted=Object.entries(data).sort((a, b) => b[1] - a[1]);
+    data=Object.fromEntries(sorted);
+    let table=`<table><tr><th>${header.charAt(0).toUpperCase() + header.slice(1)}</th><th>Proportion</th><th>People</th><th>Percent</th></tr>`;
     for (const key in data) {
         table+=`<tr><td>${key}</td><td><progress value=${data[key]/persons} max=1></progress></td><td>${data[key]}</td><td>${data[key]/persons*100}%</td></tr>`;
     }
@@ -35,25 +38,31 @@ for (const person of people) {
     } else {
         names[person.name.split(" ")[0]] = 1;
     }
+    for (const hobby of person.hobbies) {
+        if (hobbies[hobby]) {
+            hobbies[hobby] += 1;
+        } else {
+            hobbies[hobby] = 1;
+        }
+    }
 }
 
 ages_avg/=persons;
+const categories={
+    "age": ages,
+    "occupation": jobs,
+    "name": names,
+    "hobby": hobbies
+}
 
 function display(){
-    switch (document.getElementById("selected").value){
-        case "ages":
-            document.getElementById("container").innerHTML=`persons: ${persons}<br>${table("Age",ages)}average age: ${ages_avg}`;
-            break;
-        case "occupations":
-            document.getElementById("container").innerHTML=`persons: ${persons}<br>${table("Occupation",jobs)}`;
-            break;
-        case "names":
-            document.getElementById("container").innerHTML=`persons: ${persons}<br>${table("First Name",names)}`;
-            break;
-        default:
-            document.getElementById("container").innerHTML=`persons: ${persons}`;
+    if (document.getElementById("selected").value==="-"){
+        document.getElementById("container").innerHTML="<span style='color:#777;'>select data</span>";
+    }else{
+        const selected=document.getElementById("selected").value;
+        document.getElementById("container").innerHTML=table(selected,categories[selected]);
+        document.getElementById("style").innerHTML=`progress::-webkit-progress-value {background-color:${document.getElementById("color").value};}progress::-moz-progress-bar {background-color:${document.getElementById("color").value};}`;
     }
-    document.getElementById("style").innerHTML=`progress::-webkit-progress-value {background-color:${document.getElementById("color").value};}progress::-moz-progress-bar {background-color:${document.getElementById("color").value};}`;
 }
 
 window.display=display;
