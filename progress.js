168694a86e6437208867359378c14c0a2f0fa08d
mschro67 <maximilian.s107@evt.schule.koeln>
2026-02-03 08:38:33 +0000

    Update progress system

--- progress.js
+++ progress.js
@@ -1,24 +1,10 @@
 //by mschro67
-//last change: Jan 23 2026
+//last change: Feb 3 2026
 
-function c(sections,done) { //c stands for course progress in percent
-  return Math.floor(100 / sections * done);
-}
+import progress from "/data/progress.json" with {type:"json"};
 
-courses={
-  "py":c(14,14),
-  "pydev":c(9,9),
-  "html":c(5,5),
-  "swift":c(7,4.66),
-  "sql":c(6,4.42),
-  "js":c(16,9.5),
-  "back":c(9,2+0.91+0.36+0.81+0.62),
-  "front":c(15,4+0.75+0.9+0.36),
-  "css":c(4,2.06),
-  "full":c(20,4.89+0.63),
-}
-
-for (const name of Object.keys(courses)){
-  document.getElementById(name).value=courses[name]
-  document.getElementById(name+"t").innerHTML=courses[name]
+for (const course of progress){
+    document.getElementById(course["short"]).value=course["complete"];
+    document.getElementById(course["short"]).max=course["max"];
+    document.getElementById(course["short"]+"t").innerText=Math.floor(course["complete"]/course["max"]*100)
 };
